e6cc2f35dc5eff8310fed3f9b6fb070a1a277178
CBanks <banksc7669@yahoo.com>
2021-02-22 19:50:46 -0500

    debugging contact.js

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -4,7 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
-import Resume from '../../images/ChristopherBanks-WebDevelopmentResume.pdf'
+import Resume from '../../images/ChristopherBanksWebDev.jpg'
 
 function Contact() {
   return (
